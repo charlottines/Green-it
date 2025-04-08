f983faf09904f0ae44a846d71a976d4aa651905a
skiroo <skiro.fr@gmail.com>
2025-04-08 18:53:38 +0200

    Vue project creation Added Login and Register pages

---
+++ Planty/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <router-view />
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: 'App'
+    }
+</script>
+
+<style>
+    /* Style global */
+    body {
+        margin: 0;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        background-color: #f7f7f7;
+    }
+</style>
+  
