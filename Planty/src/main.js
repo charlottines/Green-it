6d38dc665e4bc4e6adb850a3898f174657d6e4f0
skiroo <skiro.fr@gmail.com>
2025-04-14 10:27:49 +0200

    Merge login and register to the navigation bar.

--- Planty/src/main.js
+++ Planty/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from "/src/App.vue"
+import router from "/src/router/index.js"
 
 createApp(App).use(router).mount('#app')
