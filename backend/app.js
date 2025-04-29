a13f87f49cfe1f57739b6b62c2f7bb16336c74ed
skiroo <skiro.fr@gmail.com>
2025-04-29 13:21:17 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,12 @@ const userRoutes = require('./routes/user');
 const groupRoutes = require('./routes/group');
 const { cleanupOldActions } = require('./routes/group');
 
-// Autoriser les requêtes CORS depuis le frontend
-app.use(cors());
+app.use(cors({
+    origin: ['https://green-it-seven.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
